dbc634150e4b181ce5de206f778bb199c9e1d516
Javatrix <javatrix912@gmail.com>
2024-09-09 13:26:17 +0200

    Telescope

---
+++ init.lua
@@ -0,0 +1,11 @@
+require("config.lazy")
+require("mappings")
+
+local opt = vim.opt
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
