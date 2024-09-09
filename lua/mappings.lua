6a0ff8aabcacd652449061d86307bdf5217f10b9
Javatrix <javatrix912@gmail.com>
2024-09-09 15:06:57 +0200

    some mappings, cmp as well

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,3 +12,18 @@ map("n", "<leader>l", vim.cmd.Lazy, { desc = "Open Lazy" })
 map("n", "<leader>x", vim.cmd.BufferClose, { desc = "Close buffer" })
 map("n", "<tab>", vim.cmd.bprev, { desc = "Next buffer" })
 map("n", "<tab>", vim.cmd.bnext, { desc = "Previous buffer" })
+
+map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+map("n", "<leader>sh", vim.lsp.buf.signature_help, { desc = "Show signature help" })
+
+map("n", "<C-h>", "<C-w>h", { desc = "Switch window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "Switch window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "Switch window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Switch window up" })
+
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "Clear highlights" })
+
+map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "Toggle line number" })
+map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "Toggle relative number" })
